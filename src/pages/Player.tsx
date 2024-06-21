23a6bf0464563bdedfd2fc730c027ed6af8f2ce5
Israel Kilday <israel.kilday@yahoo.com.br>
2024-06-20 21:30:43 -0300

    chore: add tailwind, tailwind prettier plugin, extension redux dev tools in project

---
+++ src/pages/Player.tsx
@@ -0,0 +1,9 @@
+export function Player() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
+      Player
+    </div>
+  );
+}
+
+export default Player;
