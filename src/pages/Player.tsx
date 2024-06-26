51fffe983f78c3a16593cc8f699d547c51e1a664
Israel Kilday <israel.kilday@yahoo.com.br>
2024-06-26 15:22:58 -0300

    chore: add .eluntignore

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,7 +1,18 @@
 export function Player() {
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
-      Player
+      <div className="flex flex-col gap-6 lg:w-[1100px]"></div>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-1">
+          <h1 className="font-bold lg:text-2xl">Fundamentos do Redux</h1>
+
+          <span className="text-sm text-zinc-400">
+            Módulo "Desvendando o Redux"
+          </span>
+        </div>
+      </div>
+
+      <main></main>
     </div>
   );
 }
